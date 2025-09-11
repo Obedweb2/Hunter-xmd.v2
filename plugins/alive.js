c7f378d348290bd7ee44ad7c8aee6ab8fc12ce2f
Obedweb2 <rongouniversity77@gmail.com>
2025-09-11 10:21:29 +0300

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -100,7 +100,7 @@ const alive = async (m, Matrix) => {
     },
     message: {
       contactMessage: {
-        displayName: "ᴄᴀsᴇʏʀʜᴏᴅᴇs ᴀɪ ✅",
+        displayName: "ᴏʙᴇᴅᴛᴇᴄʜ ᴀɪ ✅",
         vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:Caseyrhodes VERIFIED ✅\nORG:CASEYRHODES-TECH BOT;\nTEL;type=CELL;type=VOICE;waid=13135550002:+13135550002\nEND:VCARD`
       }
     }
